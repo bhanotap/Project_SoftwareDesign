f7507d8f822a39511cd489ee56b88aa44cba01ea
siddiade <50712861+siddiade@users.noreply.github.com>
2019-08-16 01:43:16 +0000

    Update Game.java

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -16,12 +16,14 @@ public abstract class Game
 {
     private final String gameName;//the title of the game
     private ArrayList <Player> players;// the players of the game
+    private Player p1, p2;
     
     public Game(String givenName)
-    {
+    {        
         gameName = givenName;
-        players = new ArrayList();
+        players = new ArrayList<Player>();
     }
+    
 
     /**
      * @return the gameName
@@ -38,6 +40,31 @@ public abstract class Game
     {
         return players;
     }
+
+    /**
+     * @param players the players of this game
+     */
+    public void setPlayers(ArrayList <Player> players) 
+    {
+        this.players = players;
+    }
+    
+    /**
+     * Play the game. This might be one method or many method calls depending
+     * on your game.
+     */
+    public abstract void play();
+    
+    /**
+     * When the game is over, use this method to declare and display a winning
+     * player.
+     */
+    public abstract void declareWinner();
+
+   
+    
+}//end class
+
 
     /**
      * @param players the players of this game
